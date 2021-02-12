5d8fab116fdeb51d330f972b8e08eb0c9f7dd5bc
Nishant-Karki <karkinishant14@gmail.com>
2021-02-12 14:47:58 +0545

    frontend

--- backend/index.js
+++ backend/index.js
@@ -67,9 +67,11 @@ app.get("/test", (req, res) => {
 });
 
 const verifyJWT = (req, res, next) => {
-  const token = req.headers["x-access-token"];
+  const token = req.headers["authorization"];
+  // const token = req.body.token;
+  // console.log(req.body.token);
   if (!token) {
-    res.send("Yo, we need a token.");
+    res.json({ auth: false, message: "Please provide a token." });
   } else {
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
@@ -79,64 +81,15 @@ const verifyJWT = (req, res, next) => {
         });
       } else {
         req.user_id = decoded.id;
+        console.log(user_id);
         next();
       }
     });
   }
 };
-app.get("/isUserAuth", verifyJWT, (res, req) => {
-  res.send("You are authenticated");
+app.post("/isUserAuth", verifyJWT, (req, res) => {
+  res.json({ auth: true, message: "You are authenticated" });
 });
-
-// const multer = require("multer");
-// const addServiceAPI = require("./services/addServiceAPI");
-
-// const DIR = "./uploads/";
-// const path = require("path");
-// const db = require("./database");
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, DIR);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(
-//       null,
-//       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-//     );
-//   },
-// });
-
-// const upload = multer({ storage: storage }).single("image");
-
-// app.post("/addServices", upload, (req, res) => {
-//   const { destination, filename } = req.file;
-//   const { name, description, price } = req.body;
-
-//   console.log(name);
-//   //to pass the path to the database
-//   const image_path = `${destination}${filename}`;
-
-//   try {
-//     if (!req.file) {
-//       console.log("No file received");
-//       message = "Error while image upload";
-//       res.send({ message: message, status: "danger" });
-//     } else {
-//       console.log("file received");
-//       db.query(
-//         "INSERT INTO service (name,price,description,image) VALUES (?,?,?,?)",
-//         [name, price, description, image_path],
-//         (err, result) => {
-//           err && console.log(err);
-//           result && res.send({ message: "successfully added to database" });
-//         }
-//       );
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
 
 app.listen(3001, () => {
   console.log("Server is running");
